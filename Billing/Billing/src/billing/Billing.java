9447adf72586f71e264929666a19d84d899b6698
marwa1121 <marwaelsayed1121@gmail.com>
2020-05-04 22:40:23 +0200

    modification on billing is added

--- Billing/Billing/src/billing/Billing.java
+++ Billing/Billing/src/billing/Billing.java
@@ -10,36 +10,34 @@ import java.util.List;
 
 public class Billing {
 
-   
-
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         List<String> customers = new ArrayList<String>();
-        List<Double> ratingCharges = new ArrayList<Double>();
+        List<Float> ratingCharges = new ArrayList<Float>();
         DataBase db = new DataBase();
         customers = db.getAllCustomers();
-        Double totalRatingCharges = 0.0;
-        int  monthlyFees;
-         int recurringFees;
-         int onTimeFees;
-         int customerId;
-          Double totalBill = 0.0;
-           Double totalBillWithoutTax = 0.0;
-            Double tax = 0.0;
-int profileId;
+        float totalRatingCharges = 0.0f;
+        int monthlyFees;
+        float recurringFees;
+        float onTimeFees;
+        int customerId;
+        float totalBill = 0.0f;
+        float totalBillWithoutTax = 0.0f;
+        float tax = 0.0f;
+        int profileId;
         int sizeOfCustomers = customers.size();
         int sizeOfRatingCharges = ratingCharges.size();
         for (int i = 0; i < sizeOfCustomers; i++) {
             ratingCharges = db.getRatingChargesFromUDR(customers.get(i));
             for (int y = 0; i < sizeOfRatingCharges; y++) {
                 totalRatingCharges = totalRatingCharges + ratingCharges.get(y);
-                profileId=db.getProfileIdOfCustomer(customers.get(i));
-               monthlyFees= db.getMonthlyFees( profileId);
-               recurringFees=db.getRecurringFees(profileId,4);
-               onTimeFees=db.getOnTimeFees(profileId,5);
-               totalBillWithoutTax=totalRatingCharges+monthlyFees+recurringFees+onTimeFees;
-                tax=totalBillWithoutTax*0.1f;
-                totalBill=totalBillWithoutTax+tax;
-                customerId=db.getCustomerId(customers.get(i));
+                profileId = db.getProfileIdOfCustomer(customers.get(i));
+                monthlyFees = db.getMonthlyFees(profileId);
+                recurringFees = db.getRecurringFees(profileId, 4);
+                onTimeFees = db.getOnTimeFees(profileId, 5);
+                totalBillWithoutTax = totalRatingCharges + monthlyFees + recurringFees + onTimeFees;
+                tax = totalBillWithoutTax * 0.1f;
+                totalBill = totalBillWithoutTax + tax;
+                customerId = db.getCustomerId(customers.get(i));
                 db.insertBillTable(totalBill, customerId, profileId);
             }
 
--- Billing/Billing/src/billing/DataBase.java
+++ Billing/Billing/src/billing/DataBase.java
@@ -19,7 +19,7 @@ public class DataBase {
         try {
             Class.forName("org.postgresql.Driver");
             con = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/billing", "postgres", "1234");
+                    "jdbc:postgresql://localhost:5432/billing", "postgres", "1121");
             System.out.println("connection success");
         } catch (SQLException ex) {
             System.out.println(ex);
@@ -41,15 +41,15 @@ public class DataBase {
         return customers;
 
     }
-        public List<Double> getRatingChargesFromUDR( String msisdn) throws ClassNotFoundException, SQLException {
+        public List<Float> getRatingChargesFromUDR( String msisdn) throws ClassNotFoundException, SQLException {
         connect();
         String sql = "select rating_charges from udr where dial_a= ?";
-        List<Double> ratingCharges = new ArrayList<Double>();
+        List<Float> ratingCharges = new ArrayList<Float>();
         pst = con.prepareStatement(sql);
            pst.setString(1,msisdn );
         rs = pst.executeQuery();
         while (rs.next()) {
-            ratingCharges.add(rs.getDouble("rating_charges"));
+            ratingCharges.add(rs.getFloat("rating_charges"));
         }
 
         return ratingCharges;
@@ -98,43 +98,43 @@ int customerId=0;
         return monthlyFees;
 
     }
-              public int getRecurringFees( Integer profileId ,Integer serviceId) throws ClassNotFoundException, SQLException {
+              public float getRecurringFees( Integer profileId ,Integer serviceId) throws ClassNotFoundException, SQLException {
         connect();
         String sql = "select unitcost from rating_package where profile_id= ? and service_id=?";
-        int recurringFees=0;
+        float recurringFees=0;
 
         pst = con.prepareStatement(sql);
            pst.setInt(1,profileId );
              pst.setInt(2,serviceId );
         rs = pst.executeQuery();
         while (rs.next()) {
-            recurringFees =rs.getInt("unitcost");
+            recurringFees =rs.getFloat("unitcost");
         }
 
         return recurringFees;
 
     }
-      public int getOnTimeFees( Integer profileId ,Integer serviceId) throws ClassNotFoundException, SQLException {
+      public float getOnTimeFees( Integer profileId ,Integer serviceId) throws ClassNotFoundException, SQLException {
         connect();
         String sql = "select unitcost from rating_package where profile_id= ? and service_id=?";
-        int onTimeFees=0;
+        float onTimeFees=0;
 
         pst = con.prepareStatement(sql);
            pst.setInt(1,profileId );
              pst.setInt(2,serviceId );
         rs = pst.executeQuery();
         while (rs.next()) {
-            onTimeFees =rs.getInt("unitcost");
+            onTimeFees =rs.getFloat("unitcost");
         }
 
         return onTimeFees;
 
     }
-          public void insertBillTable(Double totalCost, int customer_id, int profile_id) throws ClassNotFoundException, SQLException {
+          public void insertBillTable(float totalCost, int customer_id, int profile_id) throws ClassNotFoundException, SQLException {
         connect();
-        String sql = "insert into bill (total_cost,customer_id, profile_id,time)  Values (?,?,?,now()) ";
+        String sql = "insert into bill (total_cost,customer_id, profile_id,bill_cycle)  Values (?,?,?,now()) ";
         pst = con.prepareStatement(sql);
-        pst.setDouble(1, totalCost);
+        pst.setFloat(1, totalCost);
         pst.setInt(2,  customer_id);
         pst.setInt(3, profile_id);
         
