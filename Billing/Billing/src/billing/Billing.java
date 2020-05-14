b0b0ec1d5724fa7c0a5adccd4ada2578ed07f735
root <aya_abouelkasem@yahoo.com>
2020-05-14 20:36:14 +0200

    report

--- Billing/Billing/src/billing/Billing.java
+++ Billing/Billing/src/billing/Billing.java
@@ -1,50 +1,115 @@
 package billing;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+import org.apache.commons.collections.map.HashedMap;
 
 public class Billing {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, JRException {
         List<String> customers = new ArrayList<String>();
         List<Float> ratingCharges = new ArrayList<Float>();
+
         DataBase db = new DataBase();
+        Connection con = db.connect();
         customers = db.getAllCustomers();
-        
+        ResultSet rs;
         int monthlyFees;
         float recurringFees;
+      
         float onTimeFees;
+   
         int customerId;
         float totalBill = 0;
+        float totalDataFees = 0;
+        float totalSMSFees = 0;
+        float totalCallFees = 0;
+
         float totalBillWithoutTax = 0;
         float tax = 0;
         int profileId;
+        String customer_name = null;
+        int nid = 0;
+        int bill_id = 0;
+        String address = null;
+        float billCost = 0;
         int sizeOfCustomers = customers.size();
         for (int i = 0; i < sizeOfCustomers; i++) {
             float totalRatingCharges = 0;
             ratingCharges = db.getRatingChargesFromUDR(customers.get(i));
-                    int sizeOfRatingCharges = ratingCharges.size();
+            int sizeOfRatingCharges = ratingCharges.size();
 
-            for (int y = 0; y< sizeOfRatingCharges; y++) {
+            for (int y = 0; y < sizeOfRatingCharges; y++) {
                 totalRatingCharges = totalRatingCharges + ratingCharges.get(y);
             }
-                profileId = db.getProfileIdOfCustomer(customers.get(i));
-                monthlyFees = db.getMonthlyFees(profileId);
-                recurringFees = db.getRecurringFees(profileId, 4);
-                onTimeFees = db.getOnTimeFees(profileId, 5);
-                totalBillWithoutTax = totalRatingCharges + monthlyFees + recurringFees + onTimeFees;
-                tax =  (float)((totalBillWithoutTax) * 0.1);
-                totalBill = totalBillWithoutTax + tax;
-                customerId = db.getCustomerId(customers.get(i));
-                db.insertBillTable(totalBill, customerId, profileId);
+            profileId = db.getProfileIdOfCustomer(customers.get(i));
+            monthlyFees = db.getMonthlyFees(profileId);
+            customerId = db.getCustomerId(customers.get(i));
+            rs=db.getRecIds(customerId);
+               float totalrecurringFees=0;
+            while(rs.next()){
+             recurringFees=db.getRecCost(rs.getInt(1));
+             totalrecurringFees=totalrecurringFees+recurringFees;
+             
             }
+            rs=db.getOntimeIds(customerId);
+           float totalonTimeFees=0;
+            while(rs.next()){
+                if(rs.getBoolean(2)==false){
+                 onTimeFees=db.getOntimeCost(rs.getInt(1));
+                 totalonTimeFees=totalonTimeFees+onTimeFees;
+                 db.updateOntimeFlag(customerId,rs.getInt(1));
+                }
+            }
+            totalBillWithoutTax = totalRatingCharges + monthlyFees + totalrecurringFees+  totalonTimeFees;
+            tax = (float) ((totalBillWithoutTax) * 0.1);
+            totalBill = totalBillWithoutTax + tax;
+            
+            db.insertBillTable(totalBill, customerId, profileId);
+            bill_id = db.getBillId(customerId);
+            db.updateIs_Billing(customers.get(i));
+            db.updatebill_id(bill_id, customers.get(i));
+            totalCallFees = db.getTotalCallFees(customers.get(i));
+            totalSMSFees = db.getTotalSMSFees(customers.get(i));
+            totalDataFees = db.getTotalDataFees(customers.get(i));
+
+            JasperDesign Jd = JRXmlLoader.load("//home//aya//Downloads//Billing//BillingSystem//Billing//Billing//src//billing//report1.jrxml");
+            HashedMap param = new HashedMap();
+            param.put("p1", customerId);
+            param.put("pCallFees", totalCallFees);
+            param.put("pSMSFees", totalSMSFees);
+            param.put("pDataFees", totalDataFees);
+            param.put("pbill_id", bill_id);
+
+            param.put("pmsisdn", customers.get(i));
+
+            rs = db.getInfoOfCustomer(customerId);
+            while (rs.next()) {
+                customer_name = rs.getString("customer_name");
+            }
+            JasperReport Jr = JasperCompileManager.compileReport(Jd);
+
+            JasperPrint Jp = JasperFillManager.fillReport(Jr, param, con);
+
+            JasperViewer Jv = new JasperViewer(Jp, false);
+            Jv.setVisible(true);
+            JasperExportManager.exportReportToPdfFile(Jp, "/home//aya//Downloads//Billing//BillingSystem//bills//bill number " + bill_id + " of " + customer_name + ".pdf");
+            db.updateBillTable(bill_id);
 
         }
 
     }
 
+}
--- Billing/Billing/src/billing/DataBase.java
+++ Billing/Billing/src/billing/DataBase.java
@@ -19,7 +19,7 @@ public class DataBase {
         try {
             Class.forName("org.postgresql.Driver");
             con = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/billing", "postgres", "1121");
+                    "jdbc:postgresql://localhost:5432/billing", "postgres", "postgres");
             System.out.println("connection success");
         } catch (SQLException ex) {
             System.out.println(ex);
@@ -43,7 +43,7 @@ public class DataBase {
     }
         public List<Float> getRatingChargesFromUDR( String msisdn) throws ClassNotFoundException, SQLException {
         connect();
-        String sql = "select rating_charges from udr where dial_a= ?";
+        String sql = "select rating_charges from udr where dial_a= ? and is_billing=false";
         List<Float> ratingCharges = new ArrayList<Float>();
         pst = con.prepareStatement(sql);
            pst.setString(1,msisdn );
@@ -98,41 +98,11 @@ int customerId=0;
         return monthlyFees;
 
     }
-              public float getRecurringFees( Integer profileId ,Integer serviceId) throws ClassNotFoundException, SQLException {
-        connect();
-        String sql = "select unitcost from rating_package where profile_id= ? and service_id=?";
-        float recurringFees=0;
+  
 
-        pst = con.prepareStatement(sql);
-           pst.setInt(1,profileId );
-             pst.setInt(2,serviceId );
-        rs = pst.executeQuery();
-        while (rs.next()) {
-            recurringFees =rs.getFloat("unitcost");
-        }
-
-        return recurringFees;
-
-    }
-      public float getOnTimeFees( Integer profileId ,Integer serviceId) throws ClassNotFoundException, SQLException {
-        connect();
-        String sql = "select unitcost from rating_package where profile_id= ? and service_id=?";
-        float onTimeFees=0;
-
-        pst = con.prepareStatement(sql);
-           pst.setInt(1,profileId );
-             pst.setInt(2,serviceId );
-        rs = pst.executeQuery();
-        while (rs.next()) {
-            onTimeFees =rs.getFloat("unitcost");
-        }
-
-        return onTimeFees;
-
-    }
           public void insertBillTable(float totalCost, int customer_id, int profile_id) throws ClassNotFoundException, SQLException {
         connect();
-        String sql = "insert into bill (total_cost,customer_id, profile_id,bill_cycle)  Values (?,?,?,now()) ";
+        String sql = "insert into bill (total_cost,customer_id, profile_id,bill_cycle,Is_extracted)  Values (?,?,?,now(),false) ";
         pst = con.prepareStatement(sql);
         pst.setFloat(1, totalCost);
         pst.setInt(2,  customer_id);
@@ -140,5 +110,195 @@ int customerId=0;
         
         pst.executeUpdate();
     }
+        public ResultSet getInfoOfCustomer( int customer_id) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select customer_name,nid,address from customer where customer_id=? ";
+        pst = con.prepareStatement(sql);
+       
+        pst.setInt(1,  customer_id);
+       
+        
+        rs = pst.executeQuery();
+        return rs;
+        }
+      public float getBillCost(int customer_id) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select total_cost from bill where customer_id= ? and Is_extracted=false";
+        float billCost=0;
+
+        pst = con.prepareStatement(sql);
+           pst.setInt(1,customer_id );
+            
+        rs = pst.executeQuery();
+        while (rs.next()) {
+            billCost =rs.getFloat("total_cost");
+        }
+
+        return billCost;
+
+    }
+         public int getBillId(int customer_id  ) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select bill_id from bill where customer_id= ? and is_extracted=false";
+int billId=0;
+        pst = con.prepareStatement(sql);
+           pst.setInt(1,customer_id );
+        rs = pst.executeQuery();
+        while (rs.next()) {
+            billId=rs.getInt("bill_id");
+        }
+
+        return billId;
+
+    }
+           public void updateIs_Billing( String msisdn) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "update udr set is_billing=true  where bill_id is null and dial_a=?";
+        pst = con.prepareStatement(sql);
+      
+        pst.setString(1, msisdn);
+
+       
+        
+        pst.executeUpdate();
+    }
+       public void updatebill_id( int bill_id,String msisdn) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "update udr set bill_id=?  where bill_id is null and dial_a=?";
+        pst = con.prepareStatement(sql);
+       pst.setInt(1, bill_id);
+        pst.setString(2, msisdn);
+
+       
+        
+        pst.executeUpdate();
+    }
+        public void updateBillTable( int bill_id) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "update bill set is_extracted=true  where bill_id =? ";
+        pst = con.prepareStatement(sql);
+       pst.setInt(1, bill_id);
+       
+
+       
+        
+        pst.executeUpdate();
+    }
+    public float getTotalCallFees( String msisdn) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select sum(rating_charges) from udr where  service_id=1 and dial_a=?";
+        float totalCallFees=0;
+
+        pst = con.prepareStatement(sql);
+           pst.setString(1,msisdn);
+          
+        rs = pst.executeQuery();
+        while (rs.next()) {
+            totalCallFees =rs.getFloat(1);
+        }
+
+        return totalCallFees;
+
+    }
+        public float getTotalSMSFees( String msisdn) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select sum(rating_charges) from udr where  service_id=2 and dial_a=?";
+        float totalSMSFees=0;
+
+        pst = con.prepareStatement(sql);
+           pst.setString(1,msisdn);
+          
+        rs = pst.executeQuery();
+        while (rs.next()) {
+            totalSMSFees =rs.getFloat(1);
+        }
+
+        return totalSMSFees;
+
+    }
+         public float getTotalDataFees( String msisdn) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select sum(rating_charges) from udr where  service_id=3 and dial_a=?";
+        float totalDataFees=0;
+
+        pst = con.prepareStatement(sql);
+           pst.setString(1,msisdn);
+          
+        rs = pst.executeQuery();
+        while (rs.next()) {
+            totalDataFees=rs.getFloat(1);
+        }
+
+        return totalDataFees;
+
+    }
+   public ResultSet getRecIds( int customer_id) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select  rec_id from recurring_customer where customer_id=? ";
+        pst = con.prepareStatement(sql);
+       
+        pst.setInt(1, customer_id);
+      
+        
+        rs = pst.executeQuery();
+        return rs;
+        }
+      public float getRecCost( int rec_id) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select cost from recurring_service where  rec_id=?";
+        float recFee=0;
+
+        pst = con.prepareStatement(sql);
+           pst.setInt(1,rec_id);
+          
+        rs = pst.executeQuery();
+        while (rs.next()) {
+           recFee=rs.getFloat(1);
+        }
+
+        return recFee;
+
+    }
+   
+      public ResultSet getOntimeIds( int customer_id) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select  ontime_id, ontime_flag from ontime_customer where customer_id=? ";
+        pst = con.prepareStatement(sql);
+       
+        pst.setInt(1, customer_id);
+      
+        
+        rs = pst.executeQuery();
+        return rs;
+        }
+            public float getOntimeCost( int ontime_id) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "select cost from ontime_service where  ontime_id=?";
+        float ontimeFee=0;
+
+        pst = con.prepareStatement(sql);
+           pst.setInt(1,ontime_id);
+          
+        rs = pst.executeQuery();
+        while (rs.next()) {
+          ontimeFee=rs.getFloat(1);
+        }
+
+        return ontimeFee;
+
+    }
+         public void updateOntimeFlag( int customer_id,  int ontime_id) throws ClassNotFoundException, SQLException {
+        connect();
+        String sql = "update ontime_customer set ontime_flag=true  where customer_id =? and ontime_id=?";
+        pst = con.prepareStatement(sql);
+       pst.setInt(1, customer_id);
+       pst.setInt(2, ontime_id);
+
+       
+        
+        pst.executeUpdate();
+    }
+   
+    }
     
-}
+
